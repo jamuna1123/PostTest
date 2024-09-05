2a124b8b317ccb9c6c1d08dda3c17214b2171ea0
jamuna1123 <104769314+jamuna1123@users.noreply.github.com>
2024-09-05 18:28:28 +0545

    Update in Profie Update Form Design

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -24,7 +24,7 @@
                         <div class="card-header">
                             <div class="card-title">{{ __('Update Profile') }}</div>
                         </div>
-                        <form method="post" action="{{ route('profile.update') }}" class="fom-control mt-6 space-y-6">
+                        <form method="post" action="{{ route('profile.update') }}" class="form-control mt-6 space-y-6">
                             @csrf
                             @method('patch')
 
@@ -38,27 +38,29 @@
                                 <x-input-label for="email" :value="__('Email')" />
                                 <x-text-input id="email" name="email" type="email" class="form-control mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
                                 <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-                                @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                                    <div>
-                                        <p class="text-sm mt-2 text-gray-800">
-                                            {{ __('Your email address is unverified.') }}
-                                            <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                {{ __('Click here to re-send the verification email.') }}
-                                            </button>
-                                        </p>
-
-                                        @if (session('status') === 'verification-link-sent')
-                                            <p class="mt-2 font-medium text-sm text-green-600">
-                                                {{ __('A new verification link has been sent to your email address.') }}
-                                            </p>
-                                        @endif
-                                    </div>
-                                @endif
                             </div>
 
+                            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+                                <div>
+                                    <p class="text-sm mt-2 text-gray-800">
+                                        {{ __('Your email address is unverified.') }}
+                                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            {{ __('Click here to re-send the verification email.') }}
+                                        </button>
+                                    </p>
+
+                                    @if (session('status') === 'verification-link-sent')
+                                        <p class="mt-2 font-medium text-sm text-green-600">
+                                            {{ __('A new verification link has been sent to your email address.') }}
+                                        </p>
+                                    @endif
+                                </div>
+                            @endif
+
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> {{ __('Save') }}</button>
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa-solid fa-floppy-disk"></i> {{ __('Save') }}
+                                </button>
 
                                 @if (session('status') === 'profile-updated')
                                     <p
